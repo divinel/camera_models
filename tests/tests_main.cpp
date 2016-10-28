ac761e2d711f63041584032e10d88a7f898740bb
Robert Lukierski <robert@lukierski.eu>
2016-10-29 00:55:44 +0100

    CMake fixes

--- tests/tests_main.cpp
+++ tests/tests_main.cpp
@@ -35,21 +35,10 @@
 
 #include <gtest/gtest.h>
 
-// google logger
-#include <glog/logging.h>
-#include <gflags/gflags.h>
-
 extern "C" int main(int argc, char** argv)
 {
-    google::InitGoogleLogging(argv[0]);
-    google::LogToStderr(); // no files in /tmp
-    google::SetStderrLogging(google::INFO);
-    
     ::testing::InitGoogleTest(&argc, argv);
     int rc = RUN_ALL_TESTS();
-    
-    google::ShutdownGoogleLogging(); 
-    gflags::ShutDownCommandLineFlags();
     
     return rc;
 }
